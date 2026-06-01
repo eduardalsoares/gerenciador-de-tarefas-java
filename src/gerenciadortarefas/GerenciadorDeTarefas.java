bf92af905945896572f51fe2355dbd49852b8b3c
Eduarda Soares <dudasoa23@gmail.com>
2026-06-01 14:25:46 -0300

    feat: exclusão de comentários

--- src/gerenciadortarefas/GerenciadorDeTarefas.java
+++ src/gerenciadortarefas/GerenciadorDeTarefas.java
@@ -108,67 +108,7 @@ public class GerenciadorDeTarefas {
                 }
         } while (verificar(entrada));
     }
-
-    /* método status (Rakel)
-    public void status(Scanner entrada) {
-        do {
-            System.out.println("--------- Status das Tarefas ---------");
-            // se a opção escolhida for 3, o programa entra aqui para alterar o status de uma tarefa
-            if (listaDeTarefas.isEmpty()) { // pergunta se a lista de tarefas está vazia
-                System.out.println("Nenhuma tarefa para concluir \n"); // se a lista estiver vazia, informa ao usuário que não há tarefas para concluir                   
-            } else { // se a lista não estiver vazia, o programa entra aqui para permitir que o usuário escolha uma tarefa para concluir
-                System.out.print("Digite quantas tarefas deseja concluir: ");// pede para o usuário digitar o número da tarefa que deseja concluir
-                int index = entrada.nextInt() - 1; // lê o número da tarefa digitado pelo usuário e subtrai 1 para obter o índice correto na lista (já que a contagem começa em 0)
-
-                entrada.nextLine(); // limpa o buffer
-
-                if (index >= 0 && index <listaDeTarefas.size()) { // && significa "E" verifica se o índice digitado pelo usuário é válido (maior ou igual a 0 E menor que o tamanho da lista de tarefas)
-                    listaDeTarefas.get(index).setStatus("Concluída"); // se o índice for válido, o programa altera o status da tarefa correspondente para "Concluída" usando o método setStatus da classe Tarefa
-                    System.out.println("Tarefa Concluída!"); // informa ao usuário que a tarefa foi concluída com sucesso
-                } else { // se o índice digitado pelo usuário for inválido, o programa entra aqui para informar ao usuário que o número escolhido é inválido
-                    System.out.println("Número inválido!\n");                       
-                }
-            }
-         } while (verificar(entrada));     
-    } */
-
-    /* método editar (Rakel)
-    public void editar(Scanner entrada) {
-        do {
-            System.out.println("--------- Edição de Tarefas ---------");
-            if (listaDeTarefas.isEmpty()) { // pergunta se a lista de tarefas está vazia
-                System.out.println("Não há tarefas para editar"); // se a lista estiver vazia, informa ao usuário que não há tarefas para editar
-            } else { // se a lista não estiver vazia, o programa entra aqui para permitir que o usuário escolha uma tarefa para editar
-                System.out.println("Digite o número da tarefa que desejar editar: ");
-                int index = entrada.nextInt() -1; // lê o número da tarefa digitado pelo usuário e subtrai 1 para obter o índice correto na lista (já que a contagem começa em 0)
-                
-                entrada.nextLine(); // lipma o buffer
-
-                if (index >= 0 && index < listaDeTarefas.size()) { // verifica se o índice digitado pelo usuário é válido (maior ou igual a 0 E menor que o tamanho da lista de tarefas)
-                    Tarefa n = listaDeTarefas.get(index); // criamos uma variável n do tipo Tarefa para armazenar a tarefa selecionada pelo usuário usando o método get(index) para acessar a tarefa na lista.
-                    System.out.print("Novo Título: "); // pede para o usuário digitar o novo título da tarefa
-                    String titulo = entrada.nextLine().trim(); // lê o novo título digitado pelo usuário e remove os espaços em branco no início e no final usando o método trim()
-
-                    if (buscarPorTitulo(titulo) != null) { // se encontrou  algo é porque já existe
-                        System.out.println("Já existe uma tarefa com esse título!");
-                    } else {
-                        n.setTitulo(entrada.nextLine());// lê o novo título digitado pelo usuário e atualiza o título da tarefa usando o método setTitulo da classe Tarefa
-                                            
-                        System.out.print("Nova descrição: "); // pede para o usuário digitar a nova descrição da tarefa
-                        n.setDescricao(entrada.nextLine()); // lê a nova descrição digitada pelo usuário e atualiza a descrição da tarefa usando o método setDescricao da classe Tarefa
-                                            
-                        n.setDataLimite(lerData(entrada)); //atualiza a data limite da tarefa usando o método setDataLimite da classe Tarefa
-
-                        System.out.println("Tarefa Editada com sucesso!\n"); // informa ao usuário que a tarefa foi editada com sucesso
-                    }
-                                        
-                } else {
-                    System.out.println("Número inválido!");
-                }                      
-            }
-        } while (verificar(entrada));
-    } */
-
+    
     public void status(Scanner entrada) { // método para alterar o status de uma tarefa, que é usado para marcar tarefas como "Pendente" ou "Concluída", permitindo que o usuário acompanhe o progresso de suas tarefas e mantenha o controle sobre o que já foi feito e o que ainda precisa ser feito.
         do { 
             System.out.println("--------- Alteração de Status de Tarefas ---------");
